1a19e11944544815d90507d9c91e88b4e0b25a8c
Network Direction <luke.robertson@networkdirection.net>
2022-11-29 10:30:37 +1100

    Updated for 0.5

--- plugins/junos/junos.py
+++ plugins/junos/junos.py
@@ -7,6 +7,7 @@ Usage:
 
 Restrictions:
     Needs access to the 'teamschat' module
+    Does not support subfilters, as some other plugins do
 
 To Do:
     TBA
@@ -16,9 +17,12 @@ Author:
 """
 
 
-import sys
 import yaml
 from core import teamschat
+from core import sql
+import socket
+import struct
+from datetime import datetime
 
 
 # Location of the config file
@@ -30,14 +34,12 @@ class JunosHandler:
     def __init__(self):
         # Empty variables for later
         self.config = {}
+        self.alert_levels = {}
 
         # Read the YAML file
-        try:
-            f = open(LOCATION)
-
-            # Load the contents into alert_levels
+        with open(LOCATION) as config:
             try:
-                self.config = yaml.load(f, Loader=yaml.FullLoader)
+                self.config = yaml.load(config, Loader=yaml.FullLoader)
 
             # Handle problems with YAML syntax
             except yaml.YAMLError as err:
@@ -45,25 +47,101 @@ class JunosHandler:
                 print('Check the YAML formatting at \
                     https://yaml-online-parser.appspot.com/')
                 print(err)
-                sys.exit()
-
-        # Handle an error reading the file
-        except Exception as e:
-            print('Error opening the config file, exiting')
-            print(e)
-            sys.exit()
+                return False
 
         # Setup webhook authentication
         self.auth_header = self.config['config']['auth_header']
         self.webhook_secret = self.config['config']['webhook_secret']
 
-    def handle_event(self, raw_response, src, sql_connector):
-        # print(raw_response)
-        # print(src)
+        # Initialize alert levels
+        self.alert_levels = self.config['events']
 
-        print('Junos event:', raw_response)
-        message = f"{raw_response['message']} on \
-            <span style=\"color:Lime\"><b> \
-            {raw_response['hostname']}</b></span>"
+    # Handle the event as it comes in
+    def handle_event(self, raw_response, src):
+        # Add the sending IP to the event
+        raw_response['source'] = src
 
-        teamschat.send_chat(message)
+        # Assign a priority to the event
+        self.alert_priority(raw_response)
+
+        # Cleanup the message string
+        raw_response['message'] = \
+            raw_response['message'].replace(raw_response['event'], "")
+        raw_response['message'] = raw_response['message'].replace("'", "")
+
+        # Depending on priority,
+        # print event to terminal and prepare message for Teams
+        match raw_response['level']:
+            # Priority 1
+            case 1:
+                message = f"{raw_response['message']} on \
+                    <span style=\"color:Lime\"><b> \
+                    {raw_response['hostname']}</b></span>"
+                self.log(message, raw_response)
+
+            # Priority 2
+            case 2:
+                message = f"{raw_response['message']} on \
+                    <span style=\"color:Lime\"><b> \
+                    {raw_response['hostname']}</b></span>"
+                self.log(message, raw_response)
+
+            # Priority 3
+            case 3:
+                print('Junos event:', raw_response)
+
+            # Any other priority (4, or some error has occurred)
+            case _:
+                pass
+
+    # Assign a priority to an event
+    def alert_priority(self, webhook):
+        if webhook['event'] in self.alert_levels:
+            webhook['level'] = self.alert_levels[webhook['event']]
+        else:
+            webhook['level'] = 1
+
+    # Log to SQL and terminal
+    def log(self, message, event):
+        date = datetime.now().date()
+        time = datetime.now().time().strftime("%H:%M:%S")
+
+        chat_id = teamschat.send_chat(message)['id']
+        print('Junos event:', event)
+
+        fields = {
+            'device': f"'{event['hostname']}'",
+            'event': f"'{event['event']}'",
+            'description': f"'{event['message']}'",
+            'logdate': f"'{date}'",
+            'logtime': f"'{time}'",
+            'source': f"{ip2integer(event['source'])}",
+            'message': f"'{chat_id}'"
+        }
+
+        sql_conn = sql.Sql()
+        sql_conn.add('junos_events', fields)
+
+    # Refresh the alert levels
+    # Reread the config file
+    def refresh(self):
+        # Read the YAML file
+        with open(LOCATION) as config:
+            try:
+                self.config = yaml.load(config, Loader=yaml.FullLoader)
+
+            # Handle problems with YAML syntax
+            except yaml.YAMLError as err:
+                print('Error parsing config file, exiting')
+                print('Check the YAML formatting at \
+                    https://yaml-online-parser.appspot.com/')
+                print(err)
+                return False
+
+
+def ip2integer(ip):
+    """
+    Convert an IP string to long integer
+    """
+    packedIP = socket.inet_aton(ip)
+    return struct.unpack("!L", packedIP)[0]
